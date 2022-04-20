577b65efa26e35c22e6d8524c4ac1d6f00ff901d
Haidar <haidarally@hotmail.com>
2022-04-20 21:34:18 +0400

    Added Database Scripts

--- PG.ABBs.Calendar.Organizer.Data/Databasescript/CreateTable.sql
+++ PG.ABBs.Calendar.Organizer.Data/Databasescript/CreateTable.sql
@@ -1,38 +1,58 @@
-﻿-- Create
-
-CREATE TABLE Events(
-EventId UNIQUEIDENTIFIER,
-ContentId VARCHAR(100),
-LastUpdated DATETIME,
-Title VARCHAR(64),
-Description VARCHAR(MAX),
-Period VARCHAR(16),
-Number INT,
-URL VARCHAR(256),
-CONSTRAINT pk_events PRIMARY KEY (EventId));
-
-
-CREATE TABLE Calendar(
-CalendarId UNIQUEIDENTIFIER,
-UuidHash VARCHAR(50),
-DueDate DATETIME,
-DueDateHash VARCHAR(50),
-DateCreated DATETIME,
-CONSTRAINT pk_caldendar PRIMARY KEY (CalendarId));
-
-ALTER TABLE Calendar
-ADD Locale VARCHAR(12);
-ALTER TABLE Events
-ADD Locale VARCHAR(12);
-
-
-ALTER TABLE	Events
-ADD Type varchar(16);
-
-CREATE TABLE UserCalendar
-(UserCalendarId VARCHAR(50),
-UuidHash VARCHAR(50),
-DueDateHash VARCHAR(50),
-CalendarId VARCHAR(50),
-Locale VARCHAR(12),
-CONSTRAINT pk_UserCalendar	PRIMARY KEY (UserCalendarId));
+﻿
+/****** Object:  Table [dbo].[Calendar]    Script Date: 20/04/2022 21:23:02 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Calendar](
+	[CalendarId] [uniqueidentifier] NOT NULL,
+	[UuidHash] [nvarchar](100) NULL,
+	[DueDate] [datetime] NULL,
+	[DueDateHash] [nvarchar](100) NULL,
+	[DateCreated] [datetime] NULL,
+	[Locale] [varchar](12) NULL,
+ CONSTRAINT [pk_caldendar] PRIMARY KEY CLUSTERED 
+(
+	[CalendarId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Events]    Script Date: 20/04/2022 21:23:02 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Events](
+	[EventId] [uniqueidentifier] NOT NULL,
+	[ContentId] [varchar](100) NULL,
+	[LastCreated] [datetime] NULL,
+	[Title] [varchar](64) NULL,
+	[Description] [varchar](max) NULL,
+	[Period] [varchar](16) NULL,
+	[Number] [int] NULL,
+	[URL] [varchar](256) NULL,
+	[Locale] [varchar](12) NULL,
+	[Type] [varchar](16) NULL,
+ CONSTRAINT [pk_events] PRIMARY KEY CLUSTERED 
+(
+	[EventId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[UserCalendar]    Script Date: 20/04/2022 21:23:02 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[UserCalendar](
+	[UserCalendarId] [uniqueidentifier] NOT NULL,
+	[UuidHash] [varchar](50) NULL,
+	[DueDateHash] [nvarchar](256) NULL,
+	[CalendarId] [nvarchar](256) NULL,
+	[Locale] [varchar](12) NULL,
+ CONSTRAINT [pk_UserCalendaer] PRIMARY KEY CLUSTERED 
+(
+	[UserCalendarId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
--- PG.ABBs.Calendar.Organizer.Data/Databasescript/StoredProcedure.sql
+++ PG.ABBs.Calendar.Organizer.Data/Databasescript/StoredProcedure.sql
@@ -1,177 +1,12 @@
 ﻿
 
-/****** Object:  StoredProcedure [dbo].[[GetCalendars]]    Script Date: 14/04/2022 11:17:23 ******/
+/****** Object:  StoredProcedure [dbo].[AddOrUpdateCalendars]    Script Date: 20/04/2022 21:31:22 ******/
 SET ANSI_NULLS ON
 GO
-
 SET QUOTED_IDENTIFIER ON
 GO
 
 
-
-CREATE PROCEDURE [dbo].[GetCalendars] @locale nvarchar(max), @dueDateHash nvarchar(max),@limit int, @sorting nvarchar(max)
-AS
-
-BEGIN
-If (@limit IS NULL OR @limit = '')
-BEGIN
-SELECT * FROM Calendar WHERE Locale = @locale and DueDateHash = @dueDateHash
-ORDER BY 
-CASE WHEN @sorting = 'ASC' THEN DueDate END ASC,
-CASE WHEN @sorting = 'DESC' THEN DueDate END DESC
-END
-
-If (@sorting IS NULL OR @sorting = '')
-BEGIN
-SELECT TOP (@limit) * FROM Calendar WHERE Locale = @locale and DueDateHash = @dueDateHash
-END
-
-IF ((@sorting IS NULL OR @sorting = '')AND (@limit IS NULL OR @limit = '') )
-BEGIN
-SELECT * FROM Calendar WHERE Locale = @locale and DueDateHash = @dueDateHash
-END
-
-
-END;
-GO
-
-
-
-/****** Object:  StoredProcedure [dbo].[GetEvents]    Script Date: 14/04/2022 11:17:23 ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
-
-
-CREATE PROCEDURE [dbo].[GetEvents] @locale nvarchar(max)
-AS
-
-BEGIN
-
-IF (@locale IS NULL OR @locale = '')
-BEGIN
-SELECT * FROM Events
-END
-ELSE 
-BEGIN
-SELECT * FROM Events WHERE Locale = @locale
-END
-
-
-END;
-GO
-
-
-
-
-/****** Object:  StoredProcedure [dbo].[DeleteEvents]    Script Date: 14/04/2022 11:17:23 ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
-
-
-CREATE PROCEDURE [dbo].[DeleteEvent] @contentId nvarchar(max)
-AS
-
-BEGIN
-
-DELETE FROM Events
-WHERE ContentId = @contentId
-
-END;
-GO
-
-
-
-
-/****** Object:  StoredProcedure [dbo].[[AddOrUpdateEvents]]    Script Date: 14/04/2022 11:17:23 ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
-
-
-CREATE PROCEDURE [dbo].[AddOrUpdateEvents] 
-@contentId nvarchar(max),
-@lastUpdated DateTime,
-@title nvarchar(max),
-@description nvarchar(max),
-@period nvarchar(max),
-@number int,
-@url nvarchar(max),
-@locale nvarchar(max)
-AS
-
-BEGIN
-
-SET NOCOUNT ON
-IF(SELECT COUNT(*) FROM Events WHERE ContentId = @contentId AND Locale = @locale) > 0
-BEGIN
---UPDATE
-BEGIN TRANSACTION T1;
-	UPDATE Events
-	SET LastUpdated = @lastUpdated,
-	Title= @title,
-	Description = @description,
-	Period= @period,
-	Number = @number,
-	URL =@url
-	WHERE ContentId = @contentId AND Locale = @locale;
-COMMIT TRANSACTION T1;
-END
-ELSE
-BEGIN
-	BEGIN TRANSACTION T2;
-	INSERT INTO Events
-	(
-		EventId,
-		ContentId,
-		LastUpdated,
-		Title,
-		Description,
-		Period,
-		Number,
-		URL,
-		Locale
-	)
-	VALUES
-	(	
-		NEWID(),
-		@contentId,
-		@lastUpdated,
-		@title,
-		@description,
-		@period,
-		@number,
-		@url,
-		@locale
-		);
-	COMMIT TRANSACTION T2;
-END
-
-
-END;
-GO
-
-
-
-
-
-
-/****** Object:  StoredProcedure [dbo].[AddOrUpdateCalendars]    Script Date: 15/04/2022 14:20:08 ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
 
 
 
@@ -195,12 +30,12 @@ BEGIN TRANSACTION T1;
 	SET UuidHash = @uuidHash,
 	DueDate= @dueDate,
 	DateCreated= @dateCreated
-	WHERE CalendarId = @dueDateHash AND Locale = @locale;
+	WHERE DueDateHash = @dueDateHash AND Locale = @locale;
 COMMIT TRANSACTION T1;
 END
 ELSE
 BEGIN	
-	DECLARE @calenderId as VARCHAR;
+	DECLARE @calenderId as NVARCHAR(Max);
 	SET @calenderId = NEWID();
 	BEGIN TRANSACTION T2;
 	INSERT INTO Calendar
@@ -243,81 +78,84 @@ END
 
 END;
 GO
-
-
-
-
-
-
-/****** Object:  StoredProcedure [dbo].[[GetUserCalendars]]    Script Date: 15/04/2022 14:10:38 ******/
+/****** Object:  StoredProcedure [dbo].[AddOrUpdateEvents]    Script Date: 20/04/2022 21:31:22 ******/
 SET ANSI_NULLS ON
 GO
-
 SET QUOTED_IDENTIFIER ON
 GO
 
 
 
 
-CREATE PROCEDURE [dbo].[GetUserCalendars] @locale nvarchar(max), @uuidHash nvarchar(max),@limit int, @sorting nvarchar(max)
-AS
 
-BEGIN
-If (@limit IS NULL OR @limit = '')
-BEGIN
-SELECT * FROM Calendar WHERE Locale = @locale and DueDateHash IN (select DueDateHash from UserCalendar where UuidHash = @uuidHash)
-ORDER BY 
-CASE WHEN @sorting = 'ASC' THEN DueDate END ASC,
-CASE WHEN @sorting = 'DESC' THEN DueDate END DESC
-END
-
-If (@sorting IS NULL OR @sorting = '')
-BEGIN
-SELECT TOP (@limit) * FROM Calendar WHERE Locale = @locale and DueDateHash IN (select DueDateHash from UserCalendar where UuidHash = @uuidHash)
-END
-
-IF ((@sorting IS NULL OR @sorting = '')AND (@limit IS NULL OR @limit = '') )
-BEGIN
-SELECT * FROM Calendar WHERE Locale = @locale and DueDateHash IN (select DueDateHash from UserCalendar where UuidHash = @uuidHash)
-END
-
-
-END;
-GO
-
-
-/****** Object:  StoredProcedure [dbo].[[DeleteCalendar]]    Script Date: 15/04/2022 14:16:59 ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
-
-
-
-CREATE PROCEDURE [dbo].[DeleteCalendar] @dueDateHash nvarchar(max), @locale nvarchar(max)
+CREATE PROCEDURE [dbo].[AddOrUpdateEvents] 
+@contentId nvarchar(max),
+@lastCreated DateTime,
+@title nvarchar(max),
+@description nvarchar(max),
+@period nvarchar(max),
+@number int,
+@url nvarchar(max),
+@type nvarchar(max),
+@locale nvarchar(max)
 AS
 
 BEGIN
 
-BEGIN TRANSACTION T1
-DELETE FROM Calendar
-WHERE DueDateHash = @dueDateHash AND Locale = @locale;
+SET NOCOUNT ON
+IF(SELECT COUNT(*) FROM Events WHERE ContentId = @contentId AND Locale = @locale) > 0
+BEGIN
+--UPDATE
+BEGIN TRANSACTION T1;
+	UPDATE Events
+	SET LastCreated = @lastCreated,
+	Title= @title,
+	Description = @description,
+	Period= @period,
+	Number = @number,
+	URL =@url,
+	Type = @type
+	WHERE ContentId = @contentId AND Locale = @locale;
 COMMIT TRANSACTION T1;
+END
+ELSE
+BEGIN
+	BEGIN TRANSACTION T2;
+	INSERT INTO Events
+	(
+		EventId,
+		ContentId,
+		LastCreated,
+		Title,
+		Description,
+		Period,
+		Number,
+		URL,
+		Locale,
+		Type
+	)
+	VALUES
+	(	
+		NEWID(),
+		@contentId,
+		@lastCreated,
+		@title,
+		@description,
+		@period,
+		@number,
+		@url,
+		@locale,
+		@type
+		);
+	COMMIT TRANSACTION T2;
+END
 
-BEGIN TRANSACTION T2
-DELETE FROM UserCalendar
-WHERE DueDateHash = @dueDateHash AND Locale = @locale;
-COMMIT TRANSACTION T2;
 
 END;
 GO
-
-/****** Object:  StoredProcedure [dbo].[[AddOrUpdateUserCalendar]]    Script Date: 15/04/2022 15:19:11 ******/
+/****** Object:  StoredProcedure [dbo].[AddOrUpdateUserCalendar]    Script Date: 20/04/2022 21:31:22 ******/
 SET ANSI_NULLS ON
 GO
-
 SET QUOTED_IDENTIFIER ON
 GO
 
@@ -365,6 +203,180 @@ BEGIN
 		@locale
 		);
 	COMMIT TRANSACTION T2;
+END
+
+
+END;
+GO
+/****** Object:  StoredProcedure [dbo].[DeleteCalendar]    Script Date: 20/04/2022 21:31:22 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+
+CREATE PROCEDURE [dbo].[DeleteCalendar] @dueDateHash nvarchar(max), @locale nvarchar(max)
+AS
+
+BEGIN
+
+BEGIN TRANSACTION T1
+DELETE FROM Calendar
+WHERE DueDateHash = @dueDateHash AND Locale = @locale;
+COMMIT TRANSACTION T1;
+
+BEGIN TRANSACTION T2
+DELETE FROM UserCalendar
+WHERE DueDateHash = @dueDateHash AND Locale = @locale;
+COMMIT TRANSACTION T2;
+
+END;
+GO
+/****** Object:  StoredProcedure [dbo].[DeleteEvent]    Script Date: 20/04/2022 21:31:22 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+CREATE PROCEDURE [dbo].[DeleteEvent] @contentId nvarchar(max)
+AS
+
+BEGIN
+
+DELETE FROM Events
+WHERE ContentId = @contentId
+
+END;
+GO
+/****** Object:  StoredProcedure [dbo].[GetAllCalendars]    Script Date: 20/04/2022 21:31:22 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+
+CREATE PROCEDURE [dbo].[GetAllCalendars] @locale nvarchar(max)
+AS
+
+BEGIN
+
+IF (@locale IS NULL OR @locale = '')
+BEGIN
+SELECT * FROM Calendar
+END
+ELSE 
+BEGIN
+SELECT * FROM Calendar WHERE Locale = @locale
+END
+
+
+END;
+GO
+/****** Object:  StoredProcedure [dbo].[GetCalendars]    Script Date: 20/04/2022 21:31:22 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+CREATE PROCEDURE [dbo].[GetCalendars] @locale nvarchar(max), @dueDateHash nvarchar(max),@limit int, @sorting nvarchar(max)
+AS
+
+BEGIN
+If (@limit IS NULL OR @limit = '')
+BEGIN
+SELECT * FROM Calendar WHERE Locale = @locale and DueDateHash = @dueDateHash
+ORDER BY 
+CASE WHEN @sorting = 'ASC' THEN DueDate END ASC,
+CASE WHEN @sorting = 'DESC' THEN DueDate END DESC
+END
+
+If (@sorting IS NULL OR @sorting = '')
+BEGIN
+SELECT TOP (@limit) * FROM Calendar WHERE Locale = @locale and DueDateHash = @dueDateHash
+END
+
+IF ((@sorting IS NULL OR @sorting = '')AND (@limit IS NULL OR @limit = '') )
+BEGIN
+SELECT * FROM Calendar WHERE Locale = @locale and DueDateHash = @dueDateHash
+END
+
+
+END;
+GO
+/****** Object:  StoredProcedure [dbo].[GetEvents]    Script Date: 20/04/2022 21:31:22 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+CREATE PROCEDURE [dbo].[GetEvents] @locale nvarchar(max)
+AS
+
+BEGIN
+
+IF (@locale IS NULL OR @locale = '')
+BEGIN
+SELECT * FROM Events
+END
+ELSE 
+BEGIN
+SELECT * FROM Events WHERE Locale = @locale
+END
+
+
+END;
+GO
+/****** Object:  StoredProcedure [dbo].[GetUserCalendars]    Script Date: 20/04/2022 21:31:22 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+--exec GetUserCalendars 'en-us','e12b0498-c0a2-11ec-9d64-0242ac120002',100,'DESC';
+--SELECT * FROM Calendar
+--ORDER BY DUEDATE desc
+/*
+SELECT * FROM Calendar WHERE Locale = 'en-US' and DueDateHash IN (select DueDateHash from UserCalendar where UuidHash = 'e12b0498-c0a2-11ec-9d64-0242ac120002')
+ORDER BY DUEDATE ;
+*/
+
+
+CREATE PROCEDURE [dbo].[GetUserCalendars] @locale nvarchar(max), @uuidHash nvarchar(max),@limit int, @sorting nvarchar(max)
+AS
+
+BEGIN
+If (@limit IS NULL OR @limit = '')
+BEGIN
+--DECLARE @SQL VARCHAR(MAX);
+--SET @SQL = 'SELECT * FROM Calendar WHERE Locale = '+@locale+' and DueDateHash IN (select DueDateHash from UserCalendar where UuidHash = '+@uuidHash+')'
+--+'ORDER BY DUEDATE '+ @sorting;
+--EXECUTE sp_executesql @SQL;
+--SELECT @SQL;
+SELECT * FROM Calendar WHERE Locale = @locale and DueDateHash IN (select DueDateHash from UserCalendar where UuidHash = @uuidHash)
+GROUP BY DueDate,DateCreated,Locale,CalendarId,UuidHash,DueDateHash
+ORDER BY 
+ CASE WHEN @sorting='ASC' THEN DueDate END ASC,
+ CASE WHEN @sorting='DESC' THEN DueDate END DESC
+END
+
+If (@sorting IS NULL OR @sorting = '')
+BEGIN
+SELECT TOP (@limit) * FROM Calendar WHERE Locale = @locale and DueDateHash IN (select DueDateHash from UserCalendar where UuidHash = @uuidHash)
+END
+
+IF ((@sorting IS NULL OR @sorting = '')AND (@limit IS NULL OR @limit = '') )
+BEGIN
+SELECT * FROM Calendar WHERE Locale = @locale and DueDateHash IN (select DueDateHash from UserCalendar where UuidHash = @uuidHash)
 END
 
 
