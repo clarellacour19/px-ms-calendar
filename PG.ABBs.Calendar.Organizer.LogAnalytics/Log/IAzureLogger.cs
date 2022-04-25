fb6b1e02d202b3d1d3c82c322e038b1d494966a7
Haidar <haidarally@hotmail.com>
2022-04-25 12:04:24 +0400

    updated gitignore file

---
+++ PG.ABBs.Calendar.Organizer.LogAnalytics/Log/IAzureLogger.cs
@@ -0,0 +1,15 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IAzureLogger.cs" company="The Procter & Gamble Company">
+//   Copyright © The Procter & Gamble Company 2020 - All Rights Reserved.
+// </copyright>
+// <summary>
+//   Defines the IAzureLogger type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace PG.ABBs.Calendar.Organizer.LogAnalytics.Log
+{
+    public interface IAzureLogger
+    {
+    }
+}
