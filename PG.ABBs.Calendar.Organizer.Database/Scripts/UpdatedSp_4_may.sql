2997b9913dc4db0afedf190da5b56382d5e54162
Haidar <haidarally@hotmail.com>
2022-05-05 16:54:37 +0400

    updated appsettings updated stored procedure in db directory

---
+++ PG.ABBs.Calendar.Organizer.Database/Scripts/UpdatedSp_4_may.sql
@@ -0,0 +1,64 @@
+USE [CalendarOrganizer]
+GO
+
+/****** Object:  StoredProcedure [dbo].[GetUserCalendars]    Script Date: 04/05/2022 13:32:41 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+--exec GetUserCalendars 'en-us','e12b0498-c0a2-11ec-9d64-0242ac1200992',0,'DESC';
+--SELECT * FROM Calendar
+--ORDER BY DUEDATE desc
+/*
+SELECT * FROM Calendar WHERE Locale = 'en-US' and DueDateHash IN (select DueDateHash from UserCalendar where UuidHash = 'e12b0498-c0a2-11ec-9d64-0242ac120002')
+ORDER BY DUEDATE ;
+*/
+
+
+ALTER PROCEDURE [dbo].[GetUserCalendars] @locale nvarchar(max), @uuidHash nvarchar(max),@limit int, @sorting nvarchar(max)
+AS
+
+BEGIN
+
+If (@limit IS NULL OR @limit = 0)
+BEGIN
+--DECLARE @SQL VARCHAR(MAX);
+--SET @SQL = 'SELECT * FROM Calendar WHERE Locale = '+@locale+' and DueDateHash IN (select DueDateHash from UserCalendar where UuidHash = '+@uuidHash+')'
+--+'ORDER BY DUEDATE '+ @sorting;
+--EXECUTE sp_executesql @SQL;
+--SELECT @SQL;
+SELECT * FROM Calendar WHERE Locale = @locale and DueDateHash IN (select DueDateHash from UserCalendar where UuidHash = @uuidHash)
+--GROUP BY DueDate,DateCreated,Locale,CalendarId,UuidHash,DueDateHash
+ORDER BY 
+
+ CASE WHEN @sorting='ASC' THEN DueDate END ASC,
+ CASE WHEN @sorting = 'DESC' THEN DueDate END DESC
+END
+
+If (@sorting IS NULL OR @sorting = '')
+BEGIN
+SELECT TOP (@limit) * FROM Calendar WHERE Locale = @locale and DueDateHash IN (select DueDateHash from UserCalendar where UuidHash = @uuidHash)
+END
+
+IF ((@sorting IS NULL OR @sorting = '')AND (@limit IS NULL OR @limit = 0) )
+BEGIN
+SELECT * FROM Calendar WHERE Locale = @locale and DueDateHash IN (select DueDateHash from UserCalendar where UuidHash = @uuidHash)
+END
+
+If (@sorting IS not NULL AND @limit IS not NULL)
+BEGIN
+SELECT TOP (@limit) * FROM Calendar WHERE Locale = @locale and DueDateHash IN (select DueDateHash from UserCalendar where UuidHash = @uuidHash)
+ORDER BY 
+
+ CASE WHEN @sorting='ASC' THEN DueDate END ASC,
+ CASE WHEN @sorting = 'DESC' THEN DueDate END DESC
+END
+
+END;
+GO
+
+
